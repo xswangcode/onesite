d3ee2aa62622ec44aad1a33830c80fbd252a6a77
xswang <wxs_code@126.com>
2024-02-18 14:03:07 +0800

    一键启动配置

---
+++ deploy.bat
@@ -0,0 +1,23 @@
+chcp 65001
+@echo off
+
+if "%1" == "start" (
+    pm2 start
+) else if "%1" == "build" (
+    npm run build
+) else if "%1" == "restart" (
+    npm run build
+    pm2 delete all
+    pm2 start
+) else if "%1" == "reload" (
+    pm2 reload all
+) else if "%1" == "stop" (
+    pm2 stop all
+) else if "%1" == "delete"   (
+    pm2 delete all
+) else if "%1" == "del" (
+    pm2 delete all
+) else (
+    echo "提示: 【%1】 未定义的命令 {build|start|restart|reload|stop|delete|del}"
+)
+
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,7 +11,7 @@ module.exports = {
             script: "serve",
             env: {
                 PM2_SERVE_PATH: './dist',
-                PM2_SERVE_PORT: 8080
+                PM2_SERVE_PORT: 3000
             },
         }
     ],
