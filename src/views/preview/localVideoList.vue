b546b165dc2c7020a8151e6e9a996749b57fd6d9
xswangcode <2236785477@qq.co>
2024-08-10 17:34:53 +0800

    添加时间列

--- src/views/preview/localVideoList.vue
+++ src/views/preview/localVideoList.vue
@@ -5,11 +5,13 @@
             <el-col :span="1" style="margin-left: 20px;"> 序号 </el-col>
             <el-col :span="16" style="margin-left: 20px;"> 名字 </el-col>
             <el-col :span="3"> 大小 </el-col>
+            <el-col :span="3"> 时长 </el-col>
         </el-row> <el-row :span="24">
-            <el-col :span="1" style="margin-left: 20px;"> 序号 </el-col>
+            <el-col :span="1" style="margin-left: 20px;">  </el-col>
             <el-col :span="16" style="margin-left: 20px;">
                 <el-link type="success" @click="visit_dir('')"> <span style="font-size:25px">..</span> </el-link>
             </el-col>
+            <el-col :span="3">  </el-col>  
         </el-row>
         <el-row v-for="(item,idx) in dataList" :span="24">
             <el-col :span="1" style="margin-left: 20px;"> {{ idx+1 }} </el-col>
@@ -19,6 +21,8 @@
                 <el-link type="primary" @click="visit_dir(item.name)" v-if="item.isDirectory">{{ item.name }} 【目录】</el-link>
             </el-col>
             <el-col :span="3"> {{ item.size }} </el-col>
+            <el-col :span="3"> {{ new Date(item.createdAt).toLocaleString()  }} </el-col>
+
         </el-row>
     </div>
 </template>
