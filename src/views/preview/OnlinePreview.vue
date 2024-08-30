d55b02bf064e49c01ce8df090cbe7c84ac414a05
xswangcode <2236785477@qq.co>
2024-08-30 17:27:11 +0800

    切换tab缓存

--- src/views/preview/OnlinePreview.vue
+++ src/views/preview/OnlinePreview.vue
@@ -2,8 +2,8 @@
   <div style="body_content">
     <el-card class="col-center">
       <el-row>
-        <el-col> 
-          <el-text type="success">{{data.title}}</el-text>
+        <el-col>
+          <el-link type="success">{{ data.title }}</el-link>
         </el-col>
       </el-row>
       <el-row>
@@ -19,7 +19,7 @@
 </template>
 
 <script setup name="OnlinePreview2">
-import { ref } from 'vue'
+import { ref, onUpdated, watch } from 'vue'
 import { useRouter } from 'vue-router'
 import img404 from '../../assets/404.png'
 // //找到你的组件地址引入进来
@@ -27,45 +27,54 @@ import VideoJs from '../../components/VideoPlay.vue'
 
 // // 获取参数
 const route = useRouter()
-
 let this_href = route.currentRoute.value.params.href || 'https://cesium.com/public/SandcastleSampleData/big-buck-bunny_trailer.mp4'
 let img = route.currentRoute.value.params.img || img404
 let title = route.currentRoute.value.params.title || "这里什么也没有~"
-
 const data = ref({
   videoSrc: this_href,
   imgSrc: img,
-  title: title,
+  title: title
 })
 
+const init = () => {
+  let this_href = route.currentRoute.value.params.href || 'https://cesium.com/public/SandcastleSampleData/big-buck-bunny_trailer.mp4'
+  let img = route.currentRoute.value.params.img || img404
+  let title = route.currentRoute.value.params.title || "这里什么也没有~"
+  data.value.videoSrc = this_href
+  data.value.imgSrc = img
+  data.value.title = title
+}
+onUpdated(() => {
+  init()
+})
 
 
 </script>
 <style lang="scss" scoped>
-
 // 默认横屏
 .myVideo {
   width: 58vw;
   height: calc(58vw * 9 /16 - 10px);
-  border: 2px solid  red;
+  border: 2px solid red;
 }
 
 // 竖屏時
-@media screen and (max-width: 376px) and (max-height: 715px){ 
+@media screen and (max-width: 376px) and (max-height: 715px) {
   .myVideo {
-    height: calc(90vw * 9 /16 );
+    height: calc(90vw * 9 /16);
     width: 90vw;
-    border: 2px solid  blue;
+    border: 2px solid blue;
   }
 
-  .el-card{
+  .el-card {
     padding: 0px 0 !important;
     height: 100%;
   }
-  :deep(.el-card__body){
+
+  :deep(.el-card__body) {
     padding: 0px 0 !important;
   }
-} 
+}
 
 
 :deep(.videoPlay) {
@@ -79,11 +88,13 @@ const data = ref({
   width: 100%;
   height: 100%;
 }
-.el-card{
+
+.el-card {
   padding: 5px 0 !important;
   height: 100%;
 }
-:deep(.el-card__body){
+
+:deep(.el-card__body) {
   padding: 5px 0 !important;
 }
 </style>
